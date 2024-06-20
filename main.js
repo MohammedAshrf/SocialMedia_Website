9ff37419f7309100ce823c708c6810cf7168633e
Mohamed-Ashraf <140234307+MohammedAshrf@users.noreply.github.com>
2024-06-20 08:52:55 +0300

    save

--- main.js
+++ main.js
@@ -429,13 +429,13 @@ function deletePostConfirm() {
 }
 
 function goToProfile(userId) {
-  window.location = `/profile.html?userId=${userId}`;
+  window.location = `profile.html?userId=${userId}`;
 }
 
 function profileClicked() {
   let currentUser = getCurrentUser();
   console.log(currentUser);
-  window.location = `/profile.html?userId=${currentUser.id}`;
+  window.location = `profile.html?userId=${currentUser.id}`;
 }
 
 function toggleLoader(show = true) {
